476e06e50b141120e136eb8a3c5a4f0a88609708
Harvey Bates <phenobottle@gmail.com>
2020-12-22 23:32:53 +1100

    added day 11 solution pt1

--- 2020/Day11.cpp
+++ 2020/Day11.cpp
@@ -23,63 +23,94 @@ std::vector<std::string> get_input(const char * fileName){
 
 
 int calc_seats(std::vector<std::string> input){
-	// If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-	// If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-	// Otherwise, the seat's state does not change.
+	// Solution Part 1 (Answer = 2275)
+	std::vector<std::string> output; 
+	bool changed = true;
+	while(changed){
+		// First rule
+		for(int i=0; i < input.size(); i++){
+			std::string newLine; 
+			for(int x=0; x<input[i].length(); x++){
+				std::vector<char> adj = {input[i+1][x], input[i][x+1],
+					input[i+1][x-1], input[i+1][x+1], input[i-1][x], 
+					input[i-1][x+1], input[i-1][x-1], input[i][x-1]};
 
-	std::vector<std::string> output;
-
-	for(int i=0; i < input.size(); i++){
-		for(int x=0; x<input[i].length(); x++){
-			std::vector<char> adj = {input[i+1][x], input[i][x+1],
-				input[i+1][x-1], input[i+1][x+1]};
-
-			if(input[i][x] == 'L'){
-				bool noneAdj = true;
-				for(int y=0; y < adj.size(); y++){
-					if(adj[y] == '#'){
-						noneAdj = false;
+				if(input[i][x] == 'L'){
+					bool noneAdj = true;
+					for(int y=0; y < adj.size(); y++){
+						if(adj[y] == '#'){
+							noneAdj = false;
+						}
+					}
+					if(noneAdj){
+						newLine += '#';
+					}
+					else{
+						newLine += input[i][x];
 					}
 				}
-				if(noneAdj){
-					input[i][x] = '#';
+				else{
+					newLine += input[i][x];
 				}
 			}
+			output.push_back(newLine);
 		}
+		
+		input = output; 
+		output.clear();
+		// Second rule
+		bool seatChange = false;
+		for(int i=0; i < input.size(); i++){
+			std::string newLine;
+			for(int x=0; x<input[i].length(); x++){
+				std::vector<char> adj = {input[i+1][x], input[i][x+1],
+					input[i+1][x-1], input[i+1][x+1], input[i-1][x], 
+					input[i-1][x+1], input[i-1][x-1], input[i][x-1]};
+
+				if(input[i][x] == '#'){
+					int numAdj = 0;
+					for(int y=0; y < adj.size(); y++){
+						if(adj[y] == '#'){
+							numAdj++;
+						}
+					}
+					if(numAdj >= 4){
+						newLine += 'L';
+						seatChange = true;
+					}
+					else{
+						newLine += input[i][x];
+					}
+				}
+				else{
+					newLine += input[i][x];
+				}
+			}
+			output.push_back(newLine);
+		}
+
+		if(!seatChange){
+			changed = false;
+		}
+
+		input = output;
+		output.clear();
 	}
-
-	output = input;
-	bool seatChange = false;
-	for(int i=0; i < input.size(); i++){
-		for(int x=0; x<input[i].length(); x++){
-			std::vector<char> adj = {input[i+1][x], input[i][x+1],
-				input[i+1][x-1], input[i+1][x+1], input[i-1][x], 
-				input[i-1][x+1], input[i-1][x-1], input[i][x-1]};
-
+	int occ = 0;
+	for(int i = 0; i < input.size(); i++){
+		for(int x = 0; x < input[i].length(); x++){
 			if(input[i][x] == '#'){
-				int numAdj = 0;
-				for(int y=0; y < adj.size(); y++){
-					if(adj[y] == '#'){
-						numAdj++;
-					}
-				}
-				if(numAdj >= 4){
-					output[i][x] = 'L';
-					seatChange = true;
-				}
+				occ++;
 			}
 		}
 	}
-
-	for(int i =0; i < output.size(); i++){
-		printf("%s\n", output[i].c_str());
-	}
-	return 0;
+	return occ;
 }
 
 
 int main(){
 	std::vector<std::string> input = get_input("input_day11.txt");
-	calc_seats(input);
+	int occ = calc_seats(input);
+	printf("%d\n", occ);
 	return 0;
 }
