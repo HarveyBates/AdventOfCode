cf7aa7883961e43991842b49ec68340b145c6cdf
Harvey Bates <phenobottle@gmail.com>
2020-12-23 23:25:02 +1100

    getting my bearings

--- 2020/Day12.cpp
+++ 2020/Day12.cpp
@@ -21,15 +21,57 @@ std::vector<std::pair<char, int>> get_input(const char* fileName){
 }
 
 int manhattan_distance(std::vector<std::pair<char, int>> input){
-	int md = 0, nsPos = 0, ewPos = 0;
-	char initDir = 'E';
+	int nsPos = 0, ewPos = 0;
+
+	int heading  = 90;
 	for(int i = 0; i < input.size(); i++){
 		char direction = input[i].first;
 		int distance = input[i].second;
 
-		printf("%c %d\n", input[i].first, input[i].second);
+		switch(direction){
+			case 'F':
+				if(heading == 0){
+					nsPos += distance;
+				}
+				else if(heading == 90){
+					ewPos += distance;
+				}
+				else if(heading == 180){
+					nsPos -= distance;
+				}
+				else if(heading == 270){
+					ewPos -= distance;
+				}
+
+			case 'N':
+				heading = 0;
+				nsPos += distance;
+			case 'S':
+				heading = 180;
+				nsPos -= distance;
+			case 'E':
+				heading = 90;
+				ewPos += distance;
+			case 'W':
+				heading = 270;
+				ewPos -= distance;
+			case 'L':
+				heading -= distance;
+			case 'R':
+				heading += distance;
+		}
+
+		if(heading >= 360){
+			heading -= 360;
+		}
+		else if(heading < 0){
+			heading += 360;
+		}
+
+		printf("Input: %c%d Heading: %d EWPos: %d NSPos: %d\n", 
+			direction, distance,  heading, ewPos, nsPos);
 	}
-	return md;
+	return nsPos + ewPos;
 }
 
 
