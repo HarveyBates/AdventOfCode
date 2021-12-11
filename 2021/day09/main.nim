05f174ca3c11f9ba8f8fe0dd49d07a7c14d28964
HarveyBates <phenobottle@gmail.com>
2021-12-11 15:56:29 +1100

    added day9 pt2 2021 solution

--- 2021/day09/main.nim
+++ 2021/day09/main.nim
@@ -1,4 +1,4 @@
-import std/[strutils, sequtils, enumerate]
+import std/[strutils, sequtils, enumerate, algorithm]
 
 var heightmap = readFile("input.txt").strip().splitLines()
 
@@ -35,6 +35,67 @@ proc solve_one(): int =
         result = result + center.num + 1
 
 
-# No go for part 2 yet
 
-echo solve_one()
+proc solve_two(): int =
+  var basin = newSeq[int]()
+  var searched = newSeq[tuple[x: int, y:int]]()
+  var toSearch = newSeq[tuple[x: int, y:int]]()
+  var res = newSeq[int]()
+
+  proc eval_pos(x: int, y: int) =
+    if(x > 0):
+      if heights[y][x-1] != 9:
+        toSearch.add((x: x-1, y: y))
+    if(x < len(heights[0]) - 1):
+      if heights[y][x+1] != 9:
+        toSearch.add((x: x+1, y: y))
+    if(y > 0):
+      if heights[y-1][x] != 9:
+        toSearch.add((x: x, y: y-1))
+    if (y < len(heights) - 1):
+      if heights[y+1][x] != 9:
+        toSearch.add((x: x, y: y+1))
+  
+  for y, row in enumerate(heights):
+    for x, value in enumerate(row):
+      var center, left, right, up, down: tuple[num: int, state: bool]
+      center = (num: value, state: true)
+      if(x > 0):
+        left = (num: row[x-1], state: true)
+      if(x < len(row) - 1):
+        right = (num: row[x+1], state: true)
+      if (y > 0):
+        up = (num: heights[y-1][x], state: true)
+      if (y < heights.len - 1):
+        down = (num: heights[y+1][x], state: true)
+
+      var positions = @[left, right, up, down]
+
+      var cnt, stateCnt: int
+      for pos in positions:
+        if (center.num < pos.num):
+          inc(cnt)
+        if (pos.state):
+          inc(stateCnt)
+      if(cnt == stateCnt):
+        toSearch.add((x: x, y: y))
+        var cnt = 0
+        while( cnt < toSearch.len ):
+          var found = false
+          for completed in searched:
+            if (toSearch[cnt] == completed):
+              found = true
+              break
+          if not found:
+            eval_pos(toSearch[cnt].x, toSearch[cnt].y)
+            searched.add((toSearch[cnt].x, toSearch[cnt].y))
+          inc(cnt)
+
+        res.add(deduplicate(toSearch).len)
+        toSearch = @[]
+  res.sort()
+  result = res[^1] * res[^2] * res[^3]
+      
+  
+#echo solve_one()
+echo solve_two()
